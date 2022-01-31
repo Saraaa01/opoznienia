ed0f02ff1c29a6533a035ad9f38c3d5dc03f94a9
Saraaa01 <95320123+Saraaa01@users.noreply.github.com>
2022-01-31 10:36:17 +0100

    add package setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='opoznienia',
+    url='https://github.com/Saraaa01/opoznienia',
+    author='Sara Wawrzak',
+    author_email='263326@student.pwr.edu.pl',
+    packages=['opoznienia'],
+    install_requires=['pandas'],
+    version='0.1',
+    license='MIT',
+    description='opoznienia komunikacji',
+)
